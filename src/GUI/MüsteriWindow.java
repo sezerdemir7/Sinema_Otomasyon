118235842160c8ec01d7bd100f7856a3bfdef4f1
sezerd <ddemir1221@gmail.com>
2022-12-19 19:54:22 +0300

    s
    Co-Authored-By: SemihTemur <94394628+semih1907temur@users.noreply.github.com>

--- src/GUI/MüsteriWindow.java
+++ src/GUI/MüsteriWindow.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package GUI;
 
-/**
- *
- * @author muhdm
- */
+
 public class MüsteriWindow {
     
 }
